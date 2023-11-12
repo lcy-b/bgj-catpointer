dac4fb9dc2078c43a41d224e715bff83bac94497
Danny Sunik <danny.sunik@gmail.com>
2023-11-12 12:20:49 +0100

    final changes

--- scripts/obstacle.gd
+++ scripts/obstacle.gd
@@ -13,7 +13,7 @@ func _ready():
 		self.mass = 50
 	if random.randi_range(0, 10) > 3:
 		self.hide()
-
+		$CollisionShape2D.set_deferred("disabled", true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
