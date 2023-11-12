a9076e01c87ac13d9d1357ef00c45e7e6b07f0f8
ladis212 <ladislav.bielko@gmail.com>
2023-11-12 02:08:26 +0100

    animation and distance fix

--- scripts/cat.gd
+++ scripts/cat.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 
 var target_position = Vector2()
 var speed = 3000
+var max_anim_velocity = 1000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,13 +16,19 @@ func _integrate_forces(state):
 		var ab_vector = target_position - self.global_position
 		var direction_vector = ab_vector.normalized()
 		var force_towards_pointer = direction_vector * speed
-		var near_force = - force_towards_pointer * max(0, 1 - pow(ab_vector.length() * 0.1, 2))
+		var near_force = null
+		if ab_vector.length() < 150:
+			near_force = - force_towards_pointer
+		else:
+			near_force = Vector2(0,0)
 		var final_force = force_towards_pointer + near_force
 		apply_force(final_force)
-		$AnimatedSprite2D.play()
+		$animation.play()
+		print(state.linear_velocity.length())
+		$animation.speed_scale = state.linear_velocity.length() / max_anim_velocity * 3
 	else:
 		apply_force(Vector2(0,0))
-		$AnimatedSprite2D.stop()
+		$animation.stop()
 		
 	#look_follow(state, self.transform, target_position)
 	look_at(global_position + get_linear_velocity())
